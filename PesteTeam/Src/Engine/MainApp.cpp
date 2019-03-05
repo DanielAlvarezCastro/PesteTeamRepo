5ec178a7a1b5b057ac656a985e74d4c5b0419f68
juarui02 <juarui02@ucm.es>
2019-03-05 10:48:58 +0100

    ya tenemos ventana de renderizado

--- PesteTeam/Src/Engine/MainApp.cpp
+++ PesteTeam/Src/Engine/MainApp.cpp
@@ -11,20 +11,30 @@ MainApp::~MainApp()
 int MainApp::initApp() 
 {
 	// create root
-	Root *root = new Root();
+	Root *root; 
+
+#if _DEBUG
+	root = new Root("plugins_d.cfg");
+#else
+	root = new Root("plugins.cfg");
+#endif
+
+	root->setRenderSystem(*root->getAvailableRenderers().begin());
+
 	// choose renderer
 	if (root == NULL)
 	{
 		return 0;
 	}
+
 	// initialise root
 	root->initialise(false);
 	// create main window
 	RenderWindow *renderWindow = root->createRenderWindow("Main", 320, 240, false);
 	// create the scene
-	SceneManager *sceneMgr = root->createSceneManager("SceneManager");
+	//SceneManager *sceneMgr = root->createSceneManager("SceneManager");
 	// add a camera
-	Camera *mainCam = sceneMgr->createCamera("MainCam");
+	//Camera *mainCam = sceneMgr->createCamera("MainCam");
 	// add viewport
-	Viewport *vp = renderWindow->addViewport(mainCam);
+	//Viewport *vp = renderWindow->addViewport(mainCam);
 }
