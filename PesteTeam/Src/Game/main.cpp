9eaa9cc4295529c9b2d73bc1e1e927898c68e591
juarui02 <juarui02@ucm.es>
2019-03-12 12:36:33 +0100

    FMOD instalado, ACTUALIZAD LAS CARPETAS LIB Y DEPENDENCIES

--- PesteTeam/Src/Game/main.cpp
+++ PesteTeam/Src/Game/main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <MainApp.h>
 #include <Scene.h>
+#include <fmod_studio.hpp>
 
 int main() {
-
 	MainApp::instance()->initApp();
 	Scene* escena1 = new Scene();
 	MainApp::instance()->AddScene(escena1);
