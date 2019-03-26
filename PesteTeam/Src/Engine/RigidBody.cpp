22e04272c5b7e3f7a92605f82cb0f30489beada0
Héctor Marcos Rabadán <hmarcos@ucm.es>
2019-03-26 10:44:31 +0100

    update de rigidbody
    correccion de errores e implementacion del metodo update para la clase rigidbody.

--- PesteTeam/Src/Engine/RigidBody.cpp
+++ PesteTeam/Src/Engine/RigidBody.cpp
@@ -1,6 +1,9 @@
 #include "RigidBody.h"
 #include"GameObject.h"
 
+#include<OgreMeshManager.h>
+#include<OgreMesh.h>
+
 RigidBody::RigidBody(GameObject* gameObject_) : BasicComponent(gameObject_)
 {
 	mass = 0;
@@ -15,7 +18,7 @@ RigidBody::RigidBody(GameObject* gameObject_, btScalar mass_) : BasicComponent(g
 RigidBody::RigidBody(GameObject* gameObject_, float density, bool b) : BasicComponent(gameObject_)
 {
 	//masa = dimension del gameObject * densidad establecida
-	mass = gameObject->getScale().x * gameObject->getScale().y * gameObject->getScale().z * density;
+	mass = (gameObject->getScale().x * gameObject->getScale().y * gameObject->getScale().z) * density;
 	setIniConf();
 }
 
@@ -40,23 +43,56 @@ void RigidBody::setIniConf() {
 
 	//estado inicial del cuerpo
 	btDefaultMotionState * motionState = new btDefaultMotionState(startTransform);
-	//construimos el rigid body con toda la informacion 
+	//construimos el rigidbody con toda la informacion 
 	btRigidBody::btRigidBodyConstructionInfo rbInfo(mass, motionState, shape, localInertia);
 	rigidBody = new btRigidBody(rbInfo);
 	rigidBody->setRestitution(1);
 	rigidBody->setUserPointer(gameObject);
 	//añadimos el cuerpo al mundo fisico
 	Physics::getInstance()->addRigidBodyToWorld(rigidBody, "kk");
+
+	//debugCollider = Ogre::MeshManager::getSingleton().getByName("Cube.mesh").staticCast<Ogre::Mesh>();
 }
 
 void RigidBody::setName(const std::string newName) {
 	name = newName;
 }
 
+void RigidBody::setMass(const btScalar nmass_) {
+	//necesitamos la inercia local
+	btVector3 localInertia = rigidBody->getLocalInertia();
+	mass = nmass_;
+	//asignamos al rigidbody
+	rigidBody->setMassProps(mass, localInertia);
+}
+
 RigidBody::~RigidBody()
 {
 }
 
-void RigidBody::Update()
+void RigidBody::update(float t)
 {
+	//puntero auxiliar para ahorrar llamadas
+	btDynamicsWorld* auxWolrd = Physics::getInstance()->getDynamicWorld();
+	if (auxWolrd != NULL) {
+		//actualizamos el tiempo
+		auxWolrd->stepSimulation(t);
+		//accedemos a cada uno de los objetos
+		for (int i = 0; i < Physics::getInstance()->getNumberOfBodies(); i++) {
+			btCollisionObject* obj = auxWolrd->getCollisionObjectArray()[i];
+			btRigidBody* body = btRigidBody::upcast(obj);
+			//posicion del body
+			btTransform t;
+			if (body && body->getMotionState()) {
+				body->getMotionState()->getWorldTransform(t);
+			}
+			//control sobre el gameObject
+			btQuaternion rotation = t.getRotation();
+			gameObject->setPosition(Vec3(t.getOrigin().x(), t.getOrigin().y(), t.getOrigin().z()));
+			//sacamos los euler desde el quaternion
+			btScalar auxX, auxY, auxZ;
+			rotation.getEulerZYX(auxZ, auxY, auxX);
+			gameObject->setDirection(Vec3(auxX, auxY, auxZ));
+		}
+	}
 }
--- PesteTeam/Src/Engine/RigidBody.h
+++ PesteTeam/Src/Engine/RigidBody.h
@@ -2,9 +2,8 @@
 #define _RIGIDBODY_H
 
 #include "BasicComponent.h"
-
-
 #include"Physics.h"
+#include<Ogre.h>
 
 class RigidBody : public BasicComponent
 {
@@ -13,19 +12,25 @@ protected:
 	btRigidBody* rigidBody;
 	btScalar mass;
 
+	//para ver las lineas del collider en pantalla
+	Ogre::MeshPtr debugCollider;		
+
 	void setIniConf();
 public:
-	///constructora por defecto que deja la masa a 0
+	///constructora por defecto que deja la masa a 0 (objeto inamovible)
 	RigidBody(GameObject* gameObject_);
 	///constructora con masa por parametro
 	RigidBody(GameObject* gameObject_, btScalar mass_);
 	///contructora que calcula la masa propia con una densisad
 	RigidBody(GameObject* gameObject_, float density_, bool b=false);
 	void setName(const std::string newName);
+	void setMass(const btScalar nmass_);
 
+	void update(float t);
+
+	virtual void reciveMsg(Message* msg_) {};
 	virtual ~RigidBody();
-
-	virtual void Update();
+	virtual void Update() {};
 };
 
 #endif // _RIGIDBODY
