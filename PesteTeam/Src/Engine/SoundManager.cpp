d6f8231c7feac4d1322e57eea593bf51b41568b9
juarui02 <juarui02@ucm.es>
2019-03-29 10:59:07 +0100

    terminado motor de audio

--- PesteTeam/Src/Engine/SoundManager.cpp
+++ PesteTeam/Src/Engine/SoundManager.cpp
@@ -23,26 +23,56 @@ int SoundManager::Init()
 		return -1;
 }
 
-int SoundManager::PlaySound2D(std::string file, bool loop, bool startPaused)
+int SoundManager::Drop()
+{
+	// error if there is no engine
+	if (!engine_)
+		return -1;
+
+	// clean resources
+	for (std::list<ISound*>::const_iterator iterator = mem_sounds_.begin(), end = mem_sounds_.end(); iterator != end; ++iterator) {
+		(*iterator)->drop();
+	}
+	mem_sounds_.clear();
+
+	// delete engine
+	engine_->drop(); 
+
+	return 0;
+}
+
+ISound* SoundManager::PlaySound2D(std::string file, bool loop, bool startPaused)
 {
 	// get the final path
 	std::string finalPath = (path_ + file);
 
 	// play some sound stream
-	ISound* is_aux = engine_->play2D(finalPath.c_str(), loop, startPaused);
+	ISound* is_aux = engine_->play2D(finalPath.c_str(), loop, startPaused, true);
 
-	if (loop)
-	{
-		soundsLooping_[file] = is_aux;
-	}
+	// add it to the list so we can clean it later
+	mem_sounds_.push_back(is_aux);
 
-	return 0;
+	return is_aux;
 }
 
-int SoundManager::Drop()
+ISound* SoundManager::PlaySound3D(std::string file, vec3df pos, float minDist, bool loop, bool startPaused)
 {
-	// delete engine
-	engine_->drop(); 
+	// get the final path
+	std::string finalPath = (path_ + file);
 
-	return 0;
+	// play some sound stream on a position
+	ISound* is_aux = engine_->play3D(finalPath.c_str(), pos, loop, startPaused, true);
+
+	// set the min distance where we can hear the sound
+	is_aux->setMinDistance(minDist);
+
+	// add it to the list so we can clean it later
+	mem_sounds_.push_back(is_aux);
+
+	return is_aux;
+}
+
+void SoundManager::SetListenerPos(vec3df pos, vec3df dir)
+{
+	engine_->setListenerPosition(pos, dir);
 }
--- PesteTeam/Src/Engine/SoundManager.h
+++ PesteTeam/Src/Engine/SoundManager.h
@@ -4,11 +4,10 @@
 #include <irrKlang.h>
 #include <string>
 #include <vector>
-#include <map>
+#include <list>
+#include <iostream>
 
 using namespace irrklang;
-
-typedef std::map<std::string, ISound*> SoundMap;
 
 class SoundManager {
 private:
@@ -17,14 +16,24 @@ private:
 	ISoundEngine* engine_;
 	std::string path_ = "./Assets/Audio/";
 
-	// We store our looping sounds so we can access them one by one
-	SoundMap soundsLooping_;
+	// List of the pointers we used, we need it to clean the memory leaks
+	std::list<ISound*> mem_sounds_;
+
 public:	
+	// Constructor | Destructor
 	SoundManager();
 	~SoundManager();
+
+	// Initialize & destruct the internal engine
 	int Init();
-	int PlaySound2D(std::string file, bool loop = false, bool startPaused = false);
 	int Drop();
+
+	// Plays a sound and returns the internal variable, we can access it to change the volume, pan & stuff
+	ISound* PlaySound2D(std::string file, bool loop = false, bool startPaused = false);
+	ISound* PlaySound3D(std::string file, vec3df pos, float minDist = 0.5f, bool loop = false, bool startPaused = false);
+
+	// We have to determine where is the listener for 3D sounds
+	void SetListenerPos(vec3df pos, vec3df dir);
 
 	// Singleton logic, if not created, create the instance, else return our existing instance
 	static SoundManager* instance() {
